7aada3fe00bce5a672f9458a79b30cf73de0e88f
andrewhall1124 <andrewmartinhall2@gmail.com>
2026-09-05 08:47:02 -0400

    Reset research

--- tools/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Reusable machinery that studies import, as opposed to studies themselves.
-
-`research/<topic>/` answers a question once. Anything here is built to be used
-by more than one of them, and must not depend on any particular study.
-"""
